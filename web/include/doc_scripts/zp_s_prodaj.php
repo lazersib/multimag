9bc434bd0b69cfe657a7dd997719a72863f4a377
Blacklight <blacklight@tndproject.org>
2011-04-01 09:28:24 +0000

    - В модуле импрота данных клиента банка добавлено автоматическое определение организации и банка по расчётному счёту  - Дальнейшая разработка модуля *зарплата с продаж*

--- web/include/doc_scripts/zp_s_prodaj.php
+++ web/include/doc_scripts/zp_s_prodaj.php
@@ -56,16 +56,26 @@ function Run($mode)
 	}
 	else if($mode=='create')
 	{
+		$tov_id=rcv('tov_id');
 		$tmpl->AddText("<h1>".$this->getname()."</h1>");
-		$res=mysql_query("SELECT `curlist`.`id`, `curlist`.`user`, `doc_agent`.`name` AS `agent_name`, `curlist`.`date`, `curlist`.`sum`, `curusers`.`name` AS `ruser_name`, `zlist`.`user` AS `zuser`, `zusers`.`name` AS `zuser_name`
+		$res=mysql_query("SELECT `curlist`.`id`, `curlist`.`user`, `doc_agent`.`name` AS `agent_name`, `curlist`.`date`, `curlist`.`sum`, `curusers`.`name` AS `ruser_name`, `zlist`.`user` AS `zuser`, `zusers`.`name` AS `zuser_name`, `curlist`.`p_doc`
 		FROM `doc_list` AS `curlist`
 		INNER JOIN `doc_agent` ON		`doc_agent`.`id`=`curlist`.`agent`
 		INNER JOIN `users` AS `curusers`	ON `curusers`.`id`=`curlist`.`user`
 		LEFT JOIN `doc_list` AS `zlist`		ON `zlist`.`id`=`curlist`.`p_doc` AND `zlist`.`type`='3'
 		LEFT JOIN `users` AS `zusers`		ON `zusers`.`id`=`zlist`.`user`
-		WHERE `curlist`.`ok`>'0' AND `curlist`.`type`='2' AND `curlist`.`date`>='".strtotime("2011-03-01")."'");
+		LEFT JOIN `doc_list` AS `pay_doc`	ON `pay_doc`.`p_doc`=`curlist`.`id`
+		WHERE `curlist`.`ok`>'0' AND `curlist`.`type`='2' AND `curlist`.`date`>='".strtotime("2011-01-01")."'
+		AND `curlist`.`id` NOT IN (SELECT `doc` FROM `doc_dopdata` WHERE `param`='nzp')");
 		echo mysql_error();
-		$tmpl->AddText("<table width='100%'>
+		/// nsp (param) - начислена зарплата
+		$tmpl->AddText("
+		<form action='' method='post' enctype='multipart/form-data'>
+		<input type='hidden' name='mode' value='exec'>
+		<input type='hidden' name='param' value='i'>
+		<input type='hidden' name='sn' value='zp_s_prodaj'>
+		<input type='hidden' name='tov_id' id='tov_id' value='$tov_id'>
+		<table width='100%'>
 		<tr><th>ID<th>Автор<th>Агент<th>Дата<th>Сумма<th>К начислению");
 		$i=0;
 		$users=array();
@@ -90,6 +100,7 @@ function Run($mode)
 				}
 			
 			}
+			// Расчёт входящей стоимости
 			$res_tov=mysql_query("SELECT `doc_list_pos`.`id`, `doc_list_pos`.`tovar`, `doc_list_pos`.`cost`, `doc_list_pos`.`cnt`
 			FROM `doc_list_pos`
 			WHERE `doc_list_pos`.`doc`='{$nxt['id']}'");
@@ -99,23 +110,50 @@ function Run($mode)
 				$incost = GetInCost($nxt_tov['tovar'], $nxt['date']);
 				$nach_sum+=($nxt_tov['cost']-$incost)*$this->coeff;
 			}
+			// Проверка факта оплаты
+			$add='';
+			if($nxt['p_doc']) $add=" OR (`p_doc`='{$nxt['p_doc']}' AND (`type`='4' OR `type`='6'))";
+			$rs=mysql_query("SELECT SUM(`sum`) FROM `doc_list` WHERE 
+			(`p_doc`='{$nxt['id']}' AND (`type`='4' OR `type`='6'))
+			$add AND `ok`>0 AND `p_doc`!='0' GROUP BY `p_doc`");
+			$disable='';
+			if(@$prop=mysql_result($rs,0,0))
+			{
+				$prop=sprintf("%0.2f",$prop);
+				if($prop>=$nxt['sum'])		$cl='f_green';
+				else
+				{
+					$cl='f_brown';
+					$disable='disabled';
+				}
+			}
+			else
+			{
+				$cl='f_red';
+				$disable='disabled';
+			}
+			
 			$date=date("Y-m-d H:i:s", $nxt['date']);
-			$tmpl->AddText("<tr class='lin$i'><td><a href='/doc.php?mode=body&doc={$nxt['id']}'>{$nxt['id']}</a>
-			<td>{$nxt['ruser_name']} / {$nxt['zuser_name']}<td>{$nxt['agent_name']} <td>$date<td>{$nxt['sum']}<td><input type='text' value='$nach_sum'>");
+			$tmpl->AddText("<tr class='lin$i $cl'><td><a href='/doc.php?mode=body&doc={$nxt['id']}'>{$nxt['id']}</a>
+			<td>{$nxt['ruser_name']} / {$nxt['zuser_name']}<td>{$nxt['agent_name']} <td>$date<td>{$nxt['sum']}<td><input type='text' name='sum_doc[{$nxt['id']}]' value='$nach_sum' $disable>");
 			$i=1-$i;
-			if($nxt['zuser']>0)
-				$users[$nxt['zuser']]['sum']+=$nach_sum;
-			else	$users[$nxt['user']]['sum']+=$nach_sum;
+			if($disable=='')
+			{
+				if($nxt['zuser']>0)
+					$users[$nxt['zuser']]['sum']+=$nach_sum;
+				else	$users[$nxt['user']]['sum']+=$nach_sum;
+			}
 		}
 		$tmpl->AddText("</table>
-		Выплаты:<br>");
+		<button>Начислить зарплату</button>
+		</form>
+		Суммы выплат:<br>");
 		foreach($users as $id=>$data)
 		{
-			$tmpl->AddText("!$id!({$data['name']}) - {$data['sum']} руб.<br>");
+			$tmpl->AddText("user:$id({$data['name']}) - {$data['sum']} руб.<br>");
 		}
 	}
 }
-
 
 function getName()
 {
