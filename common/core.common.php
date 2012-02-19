bd27fd199fb0a717015a3deaba69c640a5e93241
Blacklight <blacklight@tndproject.org>
2012-02-19 15:26:24 +0000

    - Патч к базе

--- common/core.common.php
+++ common/core.common.php
@@ -17,7 +17,7 @@
 //	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-define("MULTIMAG_REV", "313");
+define("MULTIMAG_REV", "316");
 define("MULTIMAG_VERSION", "0.1r".MULTIMAG_REV);
 header("X-Powered-By: MultiMag ".MULTIMAG_VERSION);
 
