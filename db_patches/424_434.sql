d4b0019f0f5a75bd128b2ae696647405a1b92f41
Blacklight <blacklight@tndproject.org>
2012-11-09 08:15:23 +0000

    - Исправление работы с некешированными изображениями

---
+++ db_patches/424_434.sql
@@ -0,0 +1,6 @@
+INSERT INTO `users_objects` (`object`,`desc`,`actions`)VALUES ('report_mincnt', 'Отчёт по минимальному количеству', 'view');
+
+TRUNCATE `db_version`;
+INSERT INTO `db_version` (`version`) VALUES (434);
+
+
