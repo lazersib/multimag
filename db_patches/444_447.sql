f50ce6d3e691972123a036aca598054c168ba591
Blacklight <blacklight@tndproject.org>
2012-11-26 17:35:22 +0000

    - Добавлен отчёт *ведомость по агентам*

---
+++ db_patches/444_447.sql
@@ -0,0 +1,6 @@
+INSERT INTO `users_objects` (`object`,`desc`,`actions`)VALUES ('report_ved_agentov', 'Ведомость по агентам', 'view');
+
+TRUNCATE `db_version`;
+INSERT INTO `db_version` (`version`) VALUES (447);
+
+
--- web/include/reports/ved_agentov.php
+++ web/include/reports/ved_agentov.php
@@ -22,11 +22,10 @@ class Report_Ved_Agentov
 {
 	function getName($short=0)
 	{
-		if($short)	return "По Агентам";
+		if($short)	return "Ведомость по агентам";
 		else		return "Ведомость по агентам";
 	}
 	
-
 	function Form()
 	{
 		global $tmpl;
@@ -129,31 +128,97 @@ class Report_Ved_Agentov
 	function Make()
 	{
 		global $tmpl;
-		$dt_f=strtotime(@$_REQUEST['dt_f']);
-		$dt_t=strtotime(@$_REQUEST['dt_t']." 23:59:59");
-		$sel_type=@$_REQUEST['sel_type'];
+		$this->dt_f=strtotime(@$_REQUEST['dt_f']);
+		$this->dt_t=strtotime(@$_REQUEST['dt_t']." 23:59:59");
+		$this->sel_type=@$_REQUEST['sel_type'];
 		$agent=intval(@$_REQUEST['agent']);
 		$ag_sel_group=intval(@$_REQUEST['ag_sel_group']);
-		$debt=intval(@$_REQUEST['debt']);
+		$this->debt=intval(@$_REQUEST['debt']);
 		$detail_ag=intval(@$_REQUEST['detail_ag']);
-		$detail_doc=intval(@$_REQUEST['detail_doc']);
+		$this->detail_doc=intval(@$_REQUEST['detail_doc']);
 		$tmpl->AddText("<h1 id='page-title'>Ведомость по агентам</h1>
 		<table class='list' width='100%'>
-		<tr><th rowspan='2'>Дата</th><th rowspan='2'>Документ</th><th rowspan='2'>Нач.долг</th><th rowspan='2'>Увеличение долга</th><th rowspan='2'>Уменьшение долга</th><th colspan='2'>На конец периода</th></tr>
+		<tr><th rowspan='2'>Дата</th><th rowspan='2'>Документ</th><th rowspan='2'>Начальный долг</th><th rowspan='2'>Увеличение долга</th><th rowspan='2'>Уменьшение долга</th><th colspan='2'>На конец периода</th></tr>
 		<tr><th>Наш долг</th><th>Долг клиента</th></tr>");
 		
+		if($this->sel_type=='pos')
+		{
+			$res=mysql_query("SELECT `id`, `name` FROM `doc_agent` WHERE `id`='$agent' ORDER BY `name`");
+			if(mysql_errno())	throw new MysqlException("Ошибка получения списка агентов");
+			while($line=mysql_fetch_assoc($res))
+			{
+				$tmpl->AddText($this->makeBlock($line['name'], "AND `doc_list`.`agent`='{$line['id']}'"));
+			}
+		}
+		else if($this->sel_type=='group')
+		{
+			if($detail_ag)
+			{
+				$res=mysql_query("SELECT `id`, `name` FROM `doc_agent` WHERE `group`='$ag_sel_group' ORDER BY `name`");
+				if(mysql_errno())	throw new MysqlException("Ошибка получения списка агентов");
+				while($line=mysql_fetch_assoc($res))
+				{
+					$tmpl->AddText($this->makeBlock($line['name'], "AND `doc_list`.`agent`='{$line['id']}'"));
+				}
+			}
+			else
+			{
+				$res=mysql_query("SELECT `id` FROM `doc_agent` WHERE `group`='$ag_sel_group' ORDER BY `name`");
+				if(mysql_errno())	throw new MysqlException("Ошибка получения списка агентов");
+				$where='';
+				while($line=mysql_fetch_assoc($res))
+				{
+					if($where)	$where.=',';
+					$where.="OR `doc_list`.`agent`='{$line['id']}'";
+				}
+			
+				$tmpl->AddText($this->makeBlock('Выбранная группа:',"AND ($where)"));		
+			}	
+		}
+		else
+		{
+			if($detail_ag)
+			{
+				$res=mysql_query("SELECT `id`, `name` FROM `doc_agent` ORDER BY `name`");
+				if(mysql_errno())	throw new MysqlException("Ошибка получения списка агентов");
+				while($line=mysql_fetch_assoc($res))
+				{
+					$tmpl->AddText($this->makeBlock($line['name'], "AND `doc_list`.`agent`='{$line['id']}'"));
+				}
+			}
+			else
+			{
+				$tmpl->AddText($this->makeBlock('Итог:'));		
+			}
+		}
+		
+		
+		$tmpl->AddText("</table>");
+		
+	}
+	
+	function makeBlock($head,$agent_where='')
+	{
 		$res=mysql_query("SELECT `doc_list`.`id` AS `doc_id`, `doc_list`.`altnum` ,`doc_list`.`subtype`, `doc_list`.`type` AS `doc_type`, `doc_list`.`sum` AS `doc_sum`, `doc_list`.`date`, `doc_types`.`name` AS `doc_typename`, `doc_agent`.`name` AS `agent_name`
 		FROM `doc_list`
 		LEFT JOIN `doc_types` ON `doc_list`.`type`=`doc_types`.`id`
 		LEFT JOIN `doc_agent` ON `doc_agent`.`id`=`doc_list`.`agent`
-		WHERE `ok`>'0' AND `mark_del`='0' AND `date`>='$dt_f' AND `date`<='$dt_t'
+		WHERE `ok`>'0' AND `mark_del`='0' AND `date`<='{$this->dt_t}' $agent_where
 		ORDER BY `doc_list`.`date`"); 
 		if(mysql_errno())	throw new MysqlException("Ошибка получения списка документов");
-		$dolg=0;
+		$dolg=$start_dolg=$df=0;
+		$table_data=$start_dolg_p='';
 		while($line=mysql_fetch_assoc($res))
 		{
 			$d_inc=$d_dec=$info='';			
 			$cont=0;
+			if(!$df && $line['date']>$this->dt_f)
+			{
+				$df=1;
+				if($start_dolg=$dolg)
+					$start_dolg_p=sprintf("%0.2f",$dolg);
+			}
+
 			switch($line['doc_type'])
 			{
 				case 1: $dolg-=$line['doc_sum']; $d_dec=$line['doc_sum']; break;
@@ -166,22 +231,38 @@ class Report_Ved_Agentov
 				default:	$cont=1;
 			}
 			if($cont)	continue;
+			
+			$dolg_p=sprintf("%0.2f",abs($dolg));
 			$date_p=date("Y-m-d",$line['date']);
-			$startdebt='';
 			$end_nd=$end_cd='';
-			if($dolg>0)		$end_cd=$dolg;
-			else if($dolg<0)	$end_nd=-$dolg;
 			
-			if($sel_type!='pos' && $detail_ag==0)	$info=" ({$line['agent_name']})";
+			if($dolg>0)		$end_cd=$dolg_p;
+			else if($dolg<0)	$end_nd=$dolg_p;
+			if($this->sel_type!='pos' && $agent_where=='')	$info=" ({$line['agent_name']})";
 			
-			$tmpl->AddText("<tr><td>$date_p</td><td><a href='/doc.php?mode=body&amp;id={$line['doc_id']}'>{$line['doc_typename']} N{$line['altnum']}{$line['subtype']} / {$line['doc_id']}</a>{$info}</td><td>$startdebt</td>
-			<td align='right'>$d_inc</td>
-			<td align='right'>$d_dec</td>
-			<td align='right'>$end_nd</td>
-			<td align='right'>$end_cd</td></tr>");
+			if($df && $this->detail_doc)
+				$table_data.="<tr><td>$date_p</td><td><a href='/doc.php?mode=body&amp;id={$line['doc_id']}'>{$line['doc_typename']} N{$line['altnum']}{$line['subtype']} / {$line['doc_id']}</a>{$info}</td><td></td>
+				<td align='right'>$d_inc</td>
+				<td align='right'>$d_dec</td>
+				<td align='right'>$end_nd</td>
+				<td align='right'>$end_cd</td></tr>";
 		}
-		$tmpl->AddText("</table>");
 		
+		$end_cd=$end_nd=$end_cnd=$end_ccd='';
+		if($dolg>0)		$end_cd=$dolg_p;
+		else if($dolg<0)	$end_nd=$dolg_p;
+		$cdolg=$dolg-$start_dolg;
+		if($cdolg<0)		$end_ccd=sprintf("%0.2f",abs($cdolg));
+		else if($cdolg>0)	$end_cnd=sprintf("%0.2f",abs($cdolg));
+		
+		$ret='';
+		if($this->debt==0 || ($this->debt*$dolg>0))
+		{
+			$ret.="<tr style='background-color: #fec; font-weight: bold;'><td colspan='2'>$head</td><td align='right'>$start_dolg_p</td><td align='right'>$end_cnd</td><td align='right'>$end_ccd</td><td align='right' style='color: #f00;'>$end_nd</td><td align='right'>$end_cd</td></tr>";
+			if($this->detail_doc)	$ret.=$table_data;
+		}
+		
+		return $ret;
 	}
 	
 	function Run($opt)
