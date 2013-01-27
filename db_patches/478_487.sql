9ee113fe003fbe80a12907c9f597c74276b01149
Blacklight <blacklight@tndproject.org>
2013-01-27 17:33:28 +0000

    fix... спать пора...

--- db_patches/478_487.sql
+++ db_patches/478_487.sql
@@ -1,6 +1,6 @@
 INSERT INTO `users_objects` (`object`,`desc`,`actions`)VALUES ('log_call_request', 'Журнал запрошенных звонков', 'view,edit');
 
-CREATE TABLE `multimag`.`log_call_requests` (`id` INT NOT NULL AUTO_INCREMENT ,
+CREATE TABLE `log_call_requests` (`id` INT NOT NULL AUTO_INCREMENT ,
 `name` VARCHAR( 64 ) NOT NULL ,
 `phone` INT( 32 ) NOT NULL ,
 `request_date` DATETIME NOT NULL ,
