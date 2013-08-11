f73cbd8db02fd51a452fdbbe7879c339572b24f8
Blacklight <blacklight@tndproject.org>
2013-08-11 19:00:42 +0000

    - Фикс привилегий

---
+++ db_patches/562_566.sql
@@ -0,0 +1,7 @@
+INSERT INTO `users_objects` (`object`,`desc`,`actions`)VALUES ('report_reserve', 'Отчёт по резервам', 'view');
+
+
+TRUNCATE `db_version`;
+INSERT INTO `db_version` (`version`) VALUES (566);
+
+
