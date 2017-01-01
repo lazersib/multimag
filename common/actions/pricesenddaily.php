64ce8357440236e8390e0c3e0ed3d8f04c1dc467
Blacklight <blacklight@tndproject.org>
2017-01-01 21:00:29 +0000

    - Добавлены модули авторассылки

---
+++ common/actions/pricesenddaily.php
@@ -0,0 +1,39 @@
+<?php
+
+//	MultiMag v0.2 - Complex sales system
+//
+//	Copyright (C) 2005-2016, BlackLight, TND Team, http://tndproject.org
+//
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU Affero General Public License as
+//	published by the Free Software Foundation, either version 3 of the
+//	License, or (at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU Affero General Public License for more details.
+//
+//	You should have received a copy of the GNU Affero General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+namespace Actions;
+
+/// Рассылка ежедневных прайс-листов
+class pricesenddaily extends \pricesendaction {
+
+    /// Конструктор
+    public function __construct($config, $db) {
+        parent::__construct($config, $db);
+        $this->interval = self::DAILY;
+    }
+    
+    public function getName() {
+        return "Рассылка ежедневных прайс-листов";
+    }
+    
+    /// Проверить, разрешен ли периодический запуск действия
+    public function isEnabled() {
+        return \cfg::get('auto', 'pricesend');
+    }    
+}
